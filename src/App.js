34e58f2e4be830736f844ae16d1cf67043ff97b9
Smit-13 <dkorotun1@gmail.com>
2021-01-12 23:57:22 +0200

    Color Slider Added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Palette from './Palette';
 import seedColors from './seedColors';
 import {generatePalate} from './ColorHelpers';
 
+
 export class App extends Component {
   render() {
-    console.log(generatePalate(seedColors[4]));
     return (
       <div>
-        <Palette {...seedColors[1]} />
+        <Palette palette={generatePalate(seedColors[1])} />
       </div>
     );
   }
 }
-
+ 
 export default App;
